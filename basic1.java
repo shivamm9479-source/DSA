5120c3517a0f343b884c2c0e4adbc12044b29dbe
Shivam mishra <shivammishra@Shivams-MacBook-Air.local>
2026-01-03 12:14:28 +0530

    subarrays

---
+++ basic1.java
@@ -0,0 +1,20 @@
+public class basic1 {
+    public static void maximumsubarray(int numbers []){
+        int currSum = 0 ; 
+        int maxSum = Integer.MIN_VALUE;
+
+        for(int i = 0 ; i < numbers.length ; i++){
+            int start = i ; 
+            for(int j = i ; j < numbers.length ; j++){
+                int end = j ; 
+                currSum = 0 ; 
+                for(int k = start ; k <= end ; k++){
+                    currSum += numbers[k];
+                    
+                }
+            }
+        }
+    }
+
+    
+}
